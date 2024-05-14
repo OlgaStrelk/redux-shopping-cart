182e52fe5af008259fcad5b7f4dbcf4f62b9291a
“Olga <strelod@yandex.ru>
2024-05-14 21:28:16 +0300

    refactor: header

--- src/app.tsx
+++ src/app.tsx
@@ -3,18 +3,20 @@ import styles from "./app.module.css";
 import { Route, Routes } from "react-router-dom";
 import { Products } from "./features/products/products";
 import { Cart } from "./features/cart/cart";
+import Header from "./features/header/header";
 
 function App() {
   return (
-    // <div className={styles.app}>
-    <Routes>
-      <Route path="/" element={<Home />} />
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-      <Route path="/products" element={<Products />} />
+        <Route path="/products" element={<Products />} />
 
-      <Route path="/cart" element={<Cart />} />
-    </Routes>
-    // </div>
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
   );
 }
 
@@ -22,12 +24,14 @@ export default App;
 
 function Home() {
   return (
-    <main className="page">
-      <h1>Welcome to the Store</h1>
-      <figure>
-        <img src="/store.jpg" alt="A large old storefront" width="800" />
-        <figcaption>Gary Houston, CC0, via Wikimedia Commons</figcaption>
-      </figure>
-    </main>
+    <>
+      <main className="page">
+        <h1>Welcome to the Store</h1>
+        <figure>
+          <img src="/store.jpg" alt="A large old storefront" width="800" />
+          <figcaption>Gary Houston, CC0, via Wikimedia Commons</figcaption>
+        </figure>
+      </main>
+    </>
   );
 }
--- src/features/products/products.tsx
+++ src/features/products/products.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getProducts, Product } from "../../app/api";
 import styles from "./products.module.css";
+import Header from "../header/header";
 
 export function Products() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -10,27 +11,29 @@ export function Products() {
     });
   }, []);
   return (
-    <main className="page">
-      <ul className={styles.products}>
-        {products.map((product) => (
-          <li key={product.id}>
-            <article className={styles.product}>
-              <figure>
-                <img src={product.imageURL} alt={product.imageAlt} />
-                <figcaption className={styles.caption}>
-                  {product.imageCredit}
-                </figcaption>
-              </figure>
-              <div>
-                <h1>{product.name}</h1>
-                <p>{product.description}</p>
-                <p>${product.price}</p>
-                <button>Add to Cart 🛒</button>
-              </div>
-            </article>
-          </li>
-        ))}
-      </ul>
-    </main>
+    <>
+      <main className="page">
+        <ul className={styles.products}>
+          {products.map((product) => (
+            <li key={product.id}>
+              <article className={styles.product}>
+                <figure>
+                  <img src={product.imageURL} alt={product.imageAlt} />
+                  <figcaption className={styles.caption}>
+                    {product.imageCredit}
+                  </figcaption>
+                </figure>
+                <div>
+                  <h1>{product.name}</h1>
+                  <p>{product.description}</p>
+                  <p>${product.price}</p>
+                  <button>Add to Cart 🛒</button>
+                </div>
+              </article>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
   );
 }
